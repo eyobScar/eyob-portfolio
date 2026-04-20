00377c817ad492473cc3f8e19515a0f71908cd36
Eyob Alayu <eyobalayu15@gmail.com>
2026-04-21 00:13:16 +0300

    scroll position contrloller function

--- portfolio/src/Components/Navbar/Navbar.jsx
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,30 @@
 import React, { useEffect, useState } from "react";
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
+
+  const scrollPositionCtlr = (sectionIds, offset = 100) => {
+    useEffect(() => {
+      const handleScroll = () => {
+        const scrollPosition = window.scrollY + offset;
+        for (let ind = sectionIds.length - 1; i >= 0; i--) {
+          const section = document.getElementById(sectionIds[ind]);
+          if (section) {
+            const sectionTop = target.offsetTop();
+            const sectionHeight = target.offsetHeight();
+            if (scrollPosition > sectionTop && scrollPosition < sectionHeight) {
+              setActiveSection(sectionIds[ind]);
+              break;
+            }
+          }
+        }
+      };
+      handleScroll();
+      window.addEventListener("scroll".handleScroll, { passive: true });
+      return () => window.removeEventListener(scroll, handleScroll);
+    }, [sectionIds, offset]);
+    return activeSection;
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -9,7 +33,6 @@ const Navbar = () => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-  
   return <nav className=" ">Navbar</nav>;
 };
 
